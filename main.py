44132b612aadd4914b5130946be10b1112e3a89b
Ayush Singh <aayushsingh.code@gmail.com>
2021-09-30 19:46:04 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# to find the average of 5 number
+num1 = float(input("Enter 1st number:- "))
+num2 = float(input("Enter 2nd number:- "))
+num3 = float(input("Enter 3rd number:- "))
+num4 = float(input("Enter 4th number:- "))
+num5 = float(input("Enter 5th number:- "))
+
+avg = num1+num2+num3+num4+num5
+final_avg = avg/5
+
+# i have not printed anything use print statement to print final_avg
